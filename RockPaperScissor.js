bfda3eb7c083bbc803a6efd151d73a754076dca7
dr96mabuza <dr96mabuza@gmail.com>
2022-02-25 11:00:04 +0200

    game plays when buttons are selected the only thing that remains is to alert who the winner is and page refresh

--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -1,60 +1,66 @@
-const items = ["Rock", "Paper", "Scissors"];
-
 function computerPlay() {
+    const items = ["Rock", "Paper", "Scissors"];
     let item = items[Math.floor(Math.random() * items.length)];
     return item;
 }
 
+let user = 0;
+let compe = 0;
+
 function playRound(player, comp) {
     
     if (player == comp) {
-        return "Tie";
-    }else if (player == "Rock" && comp == "Scissors") {
-        return "You Win! Rock beats Scissors";
-    }
-    else if (player == "Scissors" && comp == "Paper") {
-        return "You Win! Scissors beats Paper";
-    }
-    else if (player == "Paper" && comp == "Rock") {
-        return "You Win! Paper Beats Rock";
-    }
-    else if (player == "Rock" && comp == "Paper") {
-        return "You Lose! Paper beats Rock";
-    }
-    else if (player == "Paper" && comp == "Scissors") {
-        return "You Lose! Scissors beats Paper";
-    }
-    else if (player == "Scissors" && comp == "Rock"){
-        return "You Lose! Rock beats Scissors";
+        console.log("tie");
+    }else if (player == "Rock" && comp == "Scissors" || player == "Paper" && comp == "Rock" || player == "Scissors" && comp == "Paper") {
+        return userScore.innerHTML = 1 + user++;
+    }else if (player == "Rock" && comp == "Paper" || player == "Paper" && comp == "Scissors" || player == "Scissors" && comp == "Rock") {
+        return compScore.innerHTML = 1 + compe++;
     }
 }
 
-function press() {
-    
-    function alertF1() {
-        div.innerHTML = playRound("Rock", computerPlay());
-    }
-    
-    const btn = document.querySelector('.rock');
-    btn.addEventListener('click', alertF1 );
-    
-    function alertF2() {
-        div.innerHTML = playRound("Paper", computerPlay());
-    }
-    
-    const btn2 = document.querySelector('.paper');
-    btn2.addEventListener('click', alertF2);
-    
-    function alertF3() {
-        div.innerHTML = playRound("Scissors", computerPlay());
-    }
-    
-    const btn3 = document.querySelector('.scissor');
-    btn3.addEventListener('click', alertF3);
+function alertF1() {
+    playRound("Rock", computerPlay());
 }
-
-press();
+    
+const btn = document.querySelector('.rock');
+btn.addEventListener('click', alertF1 );
+    
+function alertF2() {
+    playRound("Paper", computerPlay());
+}
+    
+const btn2 = document.querySelector('.paper');
+btn2.addEventListener('click', alertF2);
+    
+function alertF3() {
+    playRound("Scissors", computerPlay());
+}
+    
+const btn3 = document.querySelector('.scissor');
+btn3.addEventListener('click', alertF3);
 
 const content = document.querySelector('#log');
 const div = document.createElement('div');
+div.classList.add("list");
 content.appendChild(div);
+
+const line = document.createElement('p');
+content.appendChild(line);
+
+const userName = document.createElement('span');
+userName.classList.add('UserScore');
+userName.textContent = 'UserScore : ';
+line.appendChild(userName);
+
+const userScore = document.createElement('span');
+userScore.textContent = user;
+line.appendChild(userScore);
+
+const compName = document.createElement('span');
+compName.classList.add('ComputerScore');
+compName.textContent = '    ComputerScore : ';
+line.appendChild(compName);
+
+const compScore = document.createElement('span');
+compScore.textContent = compe;
+line.appendChild(compScore);
